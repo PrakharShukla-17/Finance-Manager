b81fa50c0ab6e01b5b90f6165e7c4310520b3c1a
brownPenguin43 <rishishukla17122003@gmail.com>
2025-12-02 20:51:12 +0530

    ig final cors

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,14 @@ import cors from "cors";
 
 dotenv.config();
 
-// const corsOptions = {
-//   origin: 'finance-manager-chi-inky.vercel.app', // PUT YOUR VERCEL FRONTEND URL HERE
-//   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-//   credentials: true // Allow cookies/sessions (Critical if you use cookies for login)
-// };
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "finance-manager-fawn.vercel.app"
+  ],
+  credentials: true
+}));
 
-// Allow EVERYTHING
-app.use(cors());
 app.use(express.json());
 
 
