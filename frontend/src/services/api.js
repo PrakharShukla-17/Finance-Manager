b1d3590dbc67b8e0a2b084273ba837c20fc2d227
brownPenguin43 <rishishukla17122003@gmail.com>
2025-12-02 19:50:42 +0530

    backend endpoint updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // backend root
+  baseURL: "https://finance-manager-0fv5.onrender.com", // backend root
 });
 
 // Attach token automatically if logged-in
