ba260a42ec32721bb59f62a0947719659e9caa9d
brownPenguin43 <rishishukla17122003@gmail.com>
2025-12-02 21:00:56 +0530

    final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://finance-manager-1-e1lo.onrender.com", // backend root
+  baseURL: "https://finance-manager-1-e1lo.onrender.com/api", // backend root
 });
 
 // Attach token automatically if logged-in
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) config.headers.token = `Bearer ${token}`;
   return config;
 });
 
